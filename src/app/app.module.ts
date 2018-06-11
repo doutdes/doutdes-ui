3e51651e4c5ddf07575eb6e48fbdd4ed44920097
Ibbus93 <ibbus93@gmail.com>
2018-06-11 14:19:01 +0200

    Implemented core ui angular base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+// import { MatToolbarModule, MatButtonModule } from '@angular/material';
+
+/** Nebular components **/
+/*import {
+  NbActionsModule, NbCardModule, NbLayoutModule, NbMenuItem, NbMenuModule, NbMenuService, NbSidebarModule, NbSidebarService, NbTabsetModule,
+  NbThemeModule,
+  NbUserModule
+} from '@nebular/theme';*/
 
 import { AppComponent } from './app.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { HomeComponent } from './home/home.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { LoginComponent } from './login/login.component';
+import {NbMenuInternalService} from '@nebular/theme/components/menu/menu.service';
+import {AppSidebarNavComponent} from '@coreui/angular/lib/sidebar/app-sidebar-nav.component';
+import {AppAsideModule, AppBreadcrumbModule, AppFooterModule, AppHeaderModule, AppSidebarModule} from '@coreui/angular';
+import {AppBreadcrumbService} from '@coreui/angular/lib/breadcrumb/app-breadcrumb.service';
+import {LoginComponent} from './views/login/login.component';
+import {RegisterComponent} from './views/register/register.component';
 
 const appRoutes: Routes = [
   // Qui vanno i vari percorsi dei componenti
   {
     path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
     component: HomeComponent,
     data: { title: 'DoUtDes Home' }
   },
   {
     path: 'signup',
-    component: RegistrationComponent
+    component: RegisterComponent
   },
   {
     path: 'login',
@@ -35,16 +52,22 @@ const appRoutes: Routes = [
     AppComponent,
     NotfoundComponent,
     HomeComponent,
-    RegistrationComponent,
+    RegisterComponent,
     LoginComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true }
-    )
+    AppSidebarModule,
+    AppBreadcrumbModule,
+    AppAsideModule,
+    AppHeaderModule,
+    AppFooterModule,
+    RouterModule.forRoot(appRoutes) // { enableTracing: true })
+  ],
+  exports: [
+    // MatButtonModule,
+    // MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   // users : any = require('../../../routes/users');
 
-  something: string = "Cicoria";
+  something = 'Cicoria';
 
   constructor() { }
 
