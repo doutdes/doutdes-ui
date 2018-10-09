78aaffb716a210a41a6c7a7853d8ac4dd27b7306
ibbus93@gmail.com <Kotfec@nd1>
2018-10-09 16:47:03 +0200

    Fixing spaces

--- src/app/features/calendar/calendar.module.ts
+++ src/app/features/calendar/calendar.module.ts
@@ -6,6 +6,8 @@ import {CalendarModule, DateAdapter} from 'angular-calendar';
 import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 import {CoreModule} from '../../core/core.module';
 import {CommonModule} from '@angular/common';
+import {FlatpickrModule} from 'angularx-flatpickr';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {CommonModule} from '@angular/common';
   imports: [
     CoreModule,
     CommonModule,
+    FormsModule,
     FeatureCalendarRoutingModule,
+    FlatpickrModule.forRoot(),
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
