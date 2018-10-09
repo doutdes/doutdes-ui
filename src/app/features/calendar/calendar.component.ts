4609fe03e08917215ae505634047727c7c6fcf35
ibbus93@gmail.com <Kotfec@nd1>
2018-10-09 12:15:58 +0200

    First calendar added

--- src/app/features/calendar/calendar.component.ts
+++ src/app/features/calendar/calendar.component.ts
@@ -2,12 +2,20 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Breadcrumb} from '../../core/breadcrumb/Breadcrumb';
 import {ApiKeysService} from '../../shared/_services/apikeys.service';
 import {BreadcrumbActions} from '../../core/breadcrumb/breadcrumb.actions';
+import {CalendarView} from 'angular-calendar';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-feature-calendar',
   templateUrl: './calendar.component.html',
 })
 export class FeatureCalendarComponent implements OnInit, OnDestroy{
+
+  viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Month;
+  CalendarView = CalendarView;
+  activeDayIsOpen: boolean = true;
+  refresh: Subject<any> = new Subject();
 
 
   constructor(private breadcrumbActions: BreadcrumbActions) {
@@ -32,5 +40,9 @@ export class FeatureCalendarComponent implements OnInit, OnDestroy{
 
   removeBreadcrumb() {
     this.breadcrumbActions.deleteBreadcrumb();
+  }
+
+  changeView(view) {
+    this.view = view;
   }
 }
--- src/app/features/calendar/calendar.module.ts
+++ src/app/features/calendar/calendar.module.ts
@@ -4,12 +4,16 @@ import {FeatureCalendarRoutingModule} from './calendar.routing';
 import {BreadcrumbActions} from '../../core/breadcrumb/breadcrumb.actions';
 import {CalendarModule, DateAdapter} from 'angular-calendar';
 import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
+import {CoreModule} from '../../core/core.module';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
   declarations: [
     FeatureCalendarComponent
   ],
   imports: [
+    CoreModule,
+    CommonModule,
     FeatureCalendarRoutingModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
