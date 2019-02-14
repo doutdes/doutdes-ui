b1aec8ef4bf3ef55a4d7957351c0a31484ad33d6
Ibbus93 <ibbus93@gmail.com>
2019-02-14 18:06:47 +0100

    Removing dependence

---
+++ src/app/shared/_models/InstagramData.ts
@@ -0,0 +1,16 @@
+export interface IgPage {
+  name: string;
+  id: string;
+}
+
+export interface IgAnyData {
+  value: Array<any>;
+  end_time: Date;
+}
+
+export interface IgNumberData {
+  value: number;
+  end_time: Date;
+}
+
+
