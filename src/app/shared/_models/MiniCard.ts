55015cf66a0186c86ec9bee88d66b70b8d8da016
Ibbus93 <ibbus93@gmail.com>
2019-02-28 18:03:19 +0100

    Changed icons

--- src/app/shared/_models/MiniCard.ts
+++ src/app/shared/_models/MiniCard.ts
@@ -39,22 +39,26 @@ export let FbMiniCards: MiniCard[] = [
     name: 'Fan',
     icon: 'icon-people',
     padding: 'pl-0 pr-2',
-    value: '-'
+    value: '-',
+    measure: ''
   }, {
     name: 'Post',
-    icon: 'icon-star',
+    icon: 'icon-speech',
     padding: 'pl-2 pr-sm-2 pr-0',
-    value: '-'
+    value: '-',
+    measure: ''
   }, {
     name: 'Reactions',
-    icon: 'icon-people',
+    icon: 'icon-heart',
     padding: 'pl-sm-2 pl-0 pr-2 pt-sm-0 pt-3',
-    value: '-'
+    value: '-',
+    measure: ''
   }, {
-    name: 'Copertura post',
-    icon: 'icon-clock',
+    name: 'Impressions',
+    icon: 'icon-screen-desktop',
     padding: 'pl-2 pr-0 pt-sm-0 pt-3',
-    value: '-'
+    value: '-',
+    measure: ''
   },
 ];
 export let IgMiniCards: MiniCard[] = [
