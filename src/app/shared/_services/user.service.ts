4ad43e14e1e726044a8a447c0a7e1969c3e441c5
Stefano Chessa <sr.chessa@tiscali.it>
2019-05-22 16:05:09 +0200

    added update user

--- src/app/shared/_services/user.service.ts
+++ src/app/shared/_services/user.service.ts
@@ -10,13 +10,23 @@ export class UserService {
   constructor(private http: HttpClient, private storeService: StoreService) { }
 
   register(user: User) {
-    return this.http.post(environment.protocol + environment.host + ':' + environment.port + '/users/create', user);
+    return this.http.post(this.formatUrl('create'), user)
   }
 
   get() {
     const headers = this.getAuthorization();
-    return this.http.get<User>(environment.protocol + environment.host + ':' + environment.port + '/users/getFromId', {headers});
+    // return this.http.get<User>(environment.protocol + environment.host + ':' + environment.port + '/users/getFromId', {headers});
+    return this.http.get<User>(this.formatUrl('getFromId'), {headers});
   }
+
+  update(user: User) {
+    return this.http.put(this.formatUrl('update'), user);
+  }
+
+  private formatUrl(methodName) {
+    return environment.protocol + environment.host + ':' + environment.port + '/users/' + methodName;
+  }
+
 
   private getAuthorization() {
     return new HttpHeaders()
