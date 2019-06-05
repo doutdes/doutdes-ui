c0a3be4c87c6a21a14f9f1fcd348587b75c167d1
Stefano Chessa <sr.chessa@tiscali.it>
2019-06-05 15:25:54 +0200

    added a new chart

--- src/app/shared/_services/charts_calls.service.ts
+++ src/app/shared/_services/charts_calls.service.ts
@@ -108,7 +108,11 @@ export class ChartsCallsService {
         });
         paddingRows = chartData.length % 9 ? 9 - (chartData.length % 9) : 0;
         break; // Facebook Fan City
-
+      case FB_CHART.ENGAGED_USERS: header = [['Data', 'Interazioni']];
+        for (let i = 0; i < data.length; i++) {
+          chartData.push([new Date(data[i].end_time), data[i].value]);
+        }
+        break; // Facebook Interazioni Totali
       case GA_CHART.IMPRESSIONS_DAY:
         header = [['Data', 'Visualizzazioni']];
 
@@ -610,6 +614,32 @@ export class ChartsCallsService {
           }
         };
         break;  // Fan Country Pie
+      case FB_CHART.ENGAGED_USERS:
+        formattedData = {
+          chartType: 'AreaChart',
+          dataTable: data,
+          chartClass: 5,
+          options: {
+            chartArea: {left: 0, right: 0, height: 192, top: 0},
+            legend: {position: 'none'},
+            lineWidth: data.length > 15 ? (data.length > 40 ? 2 : 3) : 4,
+            height: 210,
+            pointSize: data.length > 15 ? 0 : 7,
+            pointShape: 'circle',
+            hAxis: {gridlines: {color: 'transparent'}, textStyle: {color: '#999', fontName: 'Roboto'}, minTextSpacing: 15},
+            vAxis: {
+              gridlines: {color: '#eaeaea', count: 5},
+              minorGridlines: {color: 'transparent'},
+              minValue: this.getMinChartStep(D_TYPE.FB, data, 0.8),
+              textPosition: 'in',
+              textStyle: {color: '#999'}
+            },
+            colors: ['#8cd0bd'],
+            areaOpacity: 0.1
+          }
+        };
+
+        break;  // Fb Interazioni Totali
 
       case GA_CHART.IMPRESSIONS_DAY:
         formattedData = {
