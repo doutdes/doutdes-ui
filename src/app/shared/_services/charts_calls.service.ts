2cc9700771f5dada4b8d949e6642512bb69c0f39
marcolilliu <mash96@hotmail.it>
2019-06-07 15:57:00 +0200

    Add FB CHART: ONLINE FANS

--- src/app/shared/_services/charts_calls.service.ts
+++ src/app/shared/_services/charts_calls.service.ts
@@ -137,6 +137,14 @@ export class ChartsCallsService {
           chartData.push([new Date(data[i].end_time), data[i].value]);
         }
         break; // Facebook Feedback negativi
+      case FB_CHART.ONLINE_FANS:
+        header = [['Data', 'Fans online']];
+
+        for (let i=0; i<data.length; i++){
+          chartData.push([new Date(data[i].end_time), data[i].value]);
+        }
+
+        break; // Facebook Fan online giornalieri
 
       case GA_CHART.IMPRESSIONS_DAY:
         header = [['Data', 'Visualizzazioni']];
@@ -729,7 +737,7 @@ export class ChartsCallsService {
           dataTable: data,
           chartClass: 5,
           options: {
-            chartArea: {left: 0, right: 0, height: 192, top:0},
+            chartArea: {left: 0, right: 0, height: 192, top: 0},
             legend: {position: 'none'},
             lineWidth: data.length > 15 ? (data.length > 40 ? 2 : 3) : 4,
             height: 210,
@@ -748,6 +756,32 @@ export class ChartsCallsService {
           }
         };
         break; // Fb Feedback negativi
+      case FB_CHART.ONLINE_FANS:
+        formattedData = {
+          chartType: 'AreaChart',
+          dataTable: data,
+          chartClass: 5,
+          options: {
+            chartArea: {left: 0, right: 0, height: 192, top: 0},
+            legend: {position: 'none'},
+            lineWidth: data.length > 15 ? (data.length > 40 ? 2 : 3) : 4,
+            height: 210,
+            pointSize: data.length > 15 ? 0 : 7,
+            pointShape: 'circle',
+            hAxis: {gridlines: {color: 'transparent'}, textStyle: {color: '#999', fontName: 'Roboto'}, minTextSpacing: 15},
+            vAxis: {
+              grindLines: {color: '#eaeaea', count: 5},
+              minorGridlines: {color: 'transparent'},
+              minValue: this.getMinChartStep(D_TYPE.FB, data, 0.8),
+              textPosition: 'in',
+              textStyle: {color: '#999'}
+            },
+            colors: ['#259526'],
+            areaOpacity: 0.1
+          }
+        };
+
+        break; // Fb Fan online giornalieri
 
       case GA_CHART.IMPRESSIONS_DAY:
         formattedData = {
