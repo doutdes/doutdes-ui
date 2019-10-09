4dbb3fd1748dcde2f6347c824b189d8f024e9328
Ibbus93 <ibbus93@gmail.com>
2019-10-09 17:08:07 +0200

    Refactored facebook dir on dashboars

---
+++ src/app/features/dashboard/facebook/campaigns/facebook-campaigns.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-feature-dashboard-facebook-campaigns',
+  templateUrl: './facebook-campaigns.component.html'
+})
+
+export class FeatureDashboardFacebookCampaignsComponent {
+  constructor() {
+  }
+}
