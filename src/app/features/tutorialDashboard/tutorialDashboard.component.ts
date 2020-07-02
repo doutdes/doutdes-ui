ec5e42ea4186e1dcde87fe70327a62ae75a6bc87
StefanoChessa <sr.chessa@tiscali.it>
2020-07-02 09:39:05 +0200

    merge

---
+++ src/app/features/tutorialDashboard/tutorialDashboard.component.ts
@@ -0,0 +1,110 @@
+import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Breadcrumb} from '../../core/breadcrumb/Breadcrumb';
+import {BreadcrumbActions} from '../../core/breadcrumb/breadcrumb.actions';
+import {TranslateService} from '@ngx-translate/core';
+import {GlobalEventsManagerService} from '../../shared/_services/global-event-manager.service';
+import {StoreService} from '../../shared/_services/store.service';
+import {User} from '../../shared/_models/User';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
+import {CookieService} from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-features-tutorialDashboard',
+  templateUrl: './tutorialDashboard.component.html'
+})
+export class tutorialDashboardComponent implements OnInit, OnDestroy {
+
+  modalRef: BsModalRef;
+  lang: string;
+  value: string;
+  tmp: string;
+  user: User;
+  id: string;
+
+  @ViewChild('newVersion') newVersionTemplate: ElementRef;
+
+  constructor(
+    private cookieService: CookieService,
+    private modalService: BsModalService,
+    private breadcrumbActions: BreadcrumbActions,
+    private globalEventService: GlobalEventsManagerService,
+    public translate: TranslateService,
+    private localStore: StoreService
+  ) { }
+
+  ngOnInit() {
+    this.addBreadcrumb();
+    this.id = this.localStore.getId();
+    const cookieExists: boolean = this.cookieService.check(this.id);
+    if (!cookieExists) {
+      this.openModal(this.newVersionTemplate);
+    }
+
+  }
+
+  ngOnDestroy() {
+    this.removeBreadcrumb();
+  }
+
+  addBreadcrumb() {
+    const bread = [] as Breadcrumb[];
+
+    bread.push(new Breadcrumb('Home', '/'));
+
+    /*
+    if (this.globalEventService.getStringBreadcrumb('GUIDA_INTRO') == null) {
+
+      this.userService.get().subscribe(value => {
+        this.user = value;
+
+        this.http.get("./assets/langSetting/langBreadcrumb/" + this.conversionSetDefaultLang() + ".json")
+          .subscribe(file => {
+            this.globalEventService.langBread.next(file);
+            bread.push(new Breadcrumb(this.globalEventService.getStringBreadcrumb('GUIDA_INTRO'), '/guideIntro/'));
+          })
+      })
+
+    } else {
+      bread.push(new Breadcrumb(this.globalEventService.getStringBreadcrumb('GUIDA_INTRO'), '/guideIntro/'));
+    }
+    */
+
+    bread.push(new Breadcrumb('Guida Uso', '/tutorialDashboard/'));
+
+    this.breadcrumbActions.updateBreadcrumb(bread);
+  }
+
+  removeBreadcrumb() {
+    this.breadcrumbActions.deleteBreadcrumb();
+  }
+
+  openModal(modal: ElementRef) {
+    this.modalRef = this.modalService.show(modal,
+      {
+        class: 'modal-md modal-dialog-centered',
+        backdrop: 'static',
+        keyboard: false
+      });
+  }
+
+  closeModal() {
+    this.modalRef.hide();
+  }
+
+  conversionSetDefaultLang () {
+
+    switch (this.user.lang) {
+      case "it" :
+        this.value = "Italiano";
+        break;
+      case "en" :
+        this.value = "English";
+        break;
+      default:
+        this.value = "Italiano";
+    }
+
+    return this.value;
+  }
+
+}
---
+++ src/app/features/tutorialDashboard/tutorialDashboard.module.ts
@@ -0,0 +1,30 @@
+import {NgModule} from '@angular/core';
+import {SharedModule} from '../../shared/shared.module';
+import {CoreModule} from '../../core/core.module';
+import {BreadcrumbActions} from '../../core/breadcrumb/breadcrumb.actions';
+import {GuideIntroComponent} from '../guideIntro/guideIntro.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {UserService} from '../../shared/_services/user.service';
+import {tutorialDashboardComponent} from './tutorialDashboard.component';
+import {TutorialDashboardRoutingModule} from './tutorialDashboard.routing';
+
+@NgModule({
+  declarations: [
+    tutorialDashboardComponent
+  ],
+  imports: [
+    SharedModule,
+    CoreModule,
+    TutorialDashboardRoutingModule,
+    TranslateModule
+  ],
+  providers: [
+    UserService,
+    BreadcrumbActions
+  ],
+  exports: [
+    tutorialDashboardComponent
+  ]
+})
+
+export class FeatureTutorialDashboardModule { }
