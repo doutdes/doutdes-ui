a878ae98d8b2e262c2ce238b545037a908f1e396
marcolilliu <mash96@hotmail.it>
2020-07-06 11:46:18 +0200

    up

--- src/app/core/card/card.component.ts
+++ src/app/core/card/card.component.ts
@@ -559,6 +559,9 @@ export class CardComponent implements OnInit {
         case 28:
           window.open('./././assets/InfoGrafici/GuideSingole/Instagram/NuoviFollower.pdf', '_blank');
           break;
+        case 102:
+          window.open('./././assets/InfoGrafici/GuideSingole/Instagram/ClickInfo.pdf', '_blank');
+          break;
         default:
           this.toastr.error(this.GEService.getStringToastr(false, true, 'CARD', 'NO_INFO'),
             this.GEService.getStringToastr(true, false, 'CARD', 'NO_INFO'));
@@ -578,6 +581,7 @@ export class CardComponent implements OnInit {
       case 20:
       case 22:
       case 28:
+      case 102:
         return true;
 
       default:
